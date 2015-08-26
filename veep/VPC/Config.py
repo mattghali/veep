96ae45a66bc750b6afeb04f4a1ee4967a7ceaf1f
Matthew Ghali <mghali@snark.net>
2015-08-25 18:30:33 -0700

    get Config vars from ~/.veep.cfg config file

--- veep/VPC/Config.py
+++ veep/VPC/Config.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 
-from LocalConfig import *
+import os, sys
+from ConfigParser import SafeConfigParser
 
 
+
+self = sys.modules[__name__]
+
+# read in environment variables for config file and section
+vars = (('VEEP_CONFIG_FILE', 'cfg_path', os.path.join(os.environ['HOME'], '.veep.cfg')),
+        ('VEEP_CONFIG_PROFILE', 'cfg_section', 'DEFAULT'))
+
+for (env_name, var_name, default) in vars:
+    if os.environ.has_key(env_name):
+        setattr(self, var_name, os.environ[env_name])
+    else:
+        setattr(self, var_name, default)
+
+
+# you're going to want to override these in your ~/.veep.cfg
+home_region = 'us-west-2'
+depot = 'depot.snark.net'
+ct_bucket = 'cloudtrail.snark.net'
+
+# Other handy things to define
 blacklists = {
     'region': [ 'cn-north-1', 'us-gov-west-1' ],
     'az':     [ 'ap-northeast-1a', 'us-east-1b', 'us-west-1a' ]
@@ -11,3 +32,12 @@ blacklists = {
 userdata = ''.join([ '#!/usr/bin/env bash\n\n',
                      'yum -y install aws-cli ec2-utils\n',
                      'aws s3 cp s3://' + depot + '/boot/base - | sh -x\n'])
+
+
+# Read in values from your dotfile
+parser = SafeConfigParser()
+if parser.read(cfg_path):
+    for (name, value) in parser.items(cfg_section):
+        setattr(self, name, value)
+
+
--- veep/VPC/LocalConfig.py
+++
@@ -1,5 +0,0 @@
-# Local site config
-
-home_region = 'us-west-2'
-depot = 'depot.example.com'
-ct_bucket = 'cloudtrail.example.com'
